1099bb0acde471f408b58d2bc2bfd26f97c604d0
unknown <lecuisto56@sfr.fr>
2020-12-20 16:19:28 +0100

    escalier.js

---
+++ escalier.js
@@ -0,0 +1,12 @@
+ 
+
+ liste1 = "    # \n";
+ liste2 = "   ## \n";
+ liste3 = "  ### \n";
+ liste4 = " #### \n";
+ console.log(liste1 + liste2 + liste3 + liste4);
+
+ console.log("    #");
+ console.log("   ##");
+ console.log("  ###");
+ console.log(" ####");
